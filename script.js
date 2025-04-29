4594d3863f517c95ab38ee866c449d9f5ee28fc3
Tarkeshwar-Yadav <tky983175@gmail.com>
2025-04-29 13:41:20 +0545

    Initial commit: Responsive calculator using HTML, CSS and JavaScript

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.getElementById("calc-display");
+const buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const value = e.target.innerHTML;
+
+    if (value === "=") {
+      try {
+        string = eval(string);
+        input.value = string;
+      } catch {
+        input.value = "Error";
+        string = "";
+      }
+    } else if (value === "AC") {
+      string = "";
+      input.value = string;
+    } else if (value === "DEL") {
+      string = string.slice(0, -1);
+      input.value = string;
+    } else {
+      string += value;
+      input.value = string;
+    }
+  });
+});
